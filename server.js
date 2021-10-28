7896178e46449a5602975d7696babb091b8246d0
Joe Kowalczyk <josephdkowalczyk61@gmail.com>
2021-10-27 21:43:44 -0500

    readme update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
-const PORT = 3001;
+const PORT = process.env.PORT || 4001;
 const app = express();
 const notes = require("./db/db.json")
 
